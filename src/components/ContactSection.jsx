0c5973dac788da799692f972523128fa49a1d968
Nafla Diva Syafia <nafladiva@gmail.com>
2021-07-13 10:19:50 +0700

    update UI :lipstick:

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -9,13 +9,15 @@ function ContactSection() {
         <div className="mt-5 mb-5 container about-container">
             <Subtitle text='contact' id='contactsect' />
 
-            <div className="pt-3 text-center">
-                <h5><FontAwesomeIcon icon={faMapMarkerAlt} /> Aceh, Indonesia</h5><br />
-                <h5><FontAwesomeIcon icon={faPhoneAlt} /> +6281219500968</h5><br />
-                <h5><FontAwesomeIcon icon={faEnvelope} /> nafladiva@gmail.com</h5><br />
+            <div className="m-3 p-3 text-center card contact-card">
+                <div className="card-body">
+                    <h5><FontAwesomeIcon icon={faMapMarkerAlt} /> Aceh, Indonesia</h5><br />
+                    <h5><FontAwesomeIcon icon={faPhoneAlt} /> +6281219500968</h5><br />
+                    <h5><FontAwesomeIcon icon={faEnvelope} /> nafladiva@gmail.com</h5><br />
 
-                <Button text='Github' tag='pri' to='https://github.com/nafladiva' />
-                <Button text='Linkedin' tag='pri' to='https://www.linkedin.com/in/nafla-diva-syafia/' />
+                    <Button text='Github' tag='pri' to='https://github.com/nafladiva' />
+                    <Button text='Linkedin' tag='pri' to='https://www.linkedin.com/in/nafla-diva-syafia/' />
+                </div>
             </div>
         </div>
     )
--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -4,7 +4,7 @@ import ProjectModal from './ProjectModal'
 function ProjectBox(props) {
     return (
         <div className="col-12 col-md-6 col-lg-4 text-center">
-            <div className="p-1 card">
+            <div className="p-1 card project-card">
                 <div className="card-body">
                     <a href='/' data-bs-toggle="modal" data-bs-target={`#${props.id}`}>
                         <img className="project-img" src={props.image} alt={props.name} />
