1f89b42e219778506f86f27b95063f6ea1175eae
Nafla Diva Syafia <nafladiva@gmail.com>
2021-07-13 11:25:40 +0700

    add assets :bento:

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -5,6 +5,9 @@ import Button from './Button'
 import neumize from '../images/neumize.png'
 import finnote from '../images/finnote.png'
 import kuoto from '../images/kuoto.png'
+import muvila from '../images/muvila.png'
+import airbud from '../images/airbud.png'
+import cocheck from '../images/cocheck.png'
 
 function ProjectSection() {
     return (
@@ -15,11 +18,14 @@ function ProjectSection() {
                 <ProjectBox name='Neumize' image={neumize} url='http://www.neumize.com/' id='neumize' />
                 <ProjectBox name='Finnote' image={finnote} url='http://47.254.194.52/' id='finnote' />
                 <ProjectBox name='Kuoto' image={kuoto} id='kuoto' />
+                <ProjectBox name='Muvila' image={muvila} url='https://github.com/nafladiva/movielist-flutter' id='muvila' />
+                <ProjectBox name='Airbud' image={airbud} url='https://github.com/nafladiva/Airbud' id='airbud' />
+                <ProjectBox name='Co-Check' image={cocheck} url='https://github.com/nafladiva/co-check' id='cocheck' />
             </div>
 
             <div className="text-center">
                 <p className="mt-5 text-center">more project still on going...</p>
-                <Button text='See more' tag='sec' to='#' />
+                {/* <Button text='See more' tag='sec' to='' /> */}
             </div>
         </div>
     )
--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -16,6 +16,24 @@ const projects = [
         name: 'Kuoto',
         description: 'Kuoto is a website for generating random quotes from Public API. This website is made for completing the course in Dicoding Indonesia (Belajar Fundamental Front-End Web Development). Kuoto was made by using ES6, Web Component, and Build Tools as taught in the course.',
         url: '#'
+    },
+    {
+        id: 'muvila',
+        name: 'Muvila',
+        description: 'Muvila is a simple app that provide information about movies. This app also has feature for randomize movie that could be a choice for you to watch, if you are confused about choosing. Muvila is built by using Flutter.',
+        url: 'https://github.com/nafladiva/movielist-flutter'
+    },
+    {
+        id: 'airbud',
+        name: 'Airbud',
+        description: 'Airbud is a website where we can see the level of quality of air in certain areas/city. This website is built by using Django.',
+        url: 'https://github.com/nafladiva/Airbud'
+    },
+    {
+        id: 'cocheck',
+        name: 'Co-Check',
+        description: 'Co-Check is a simple android app which has a feature in the form of checking their condition based on the symptoms that the user has, so that they know what to do next. This Co-Check application also has a feature to inform the latest total cases regarding Covid-19, that obtained from public API. This app is built by using Java.',
+        url: 'https://github.com/nafladiva/co-check'
     }
 ]
 
