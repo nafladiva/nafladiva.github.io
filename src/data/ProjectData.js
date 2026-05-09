a0761bd1786ddcfa77ea66e0ab6cbddaca1802a6
Nafla Diva Syafia <nafladiva@gmail.com>
2026-05-09 22:06:51 +0700

    feat: add tab section in filter in project section

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -3,79 +3,92 @@ const projects = [
         id: 'podipu',
         name: 'Podipu',
         description: 'A Podcast Mobile App, where you can listen to a variety of the best podcasts, explore podcasts by genres, save podcasts you want to listen to later, and much more!. Podipu was built with Flutter, Bloc, Hive and powered by ListenNotes API for all podcast data.',
-        url: 'https://github.com/nafladiva/podipu'
+        url: 'https://github.com/nafladiva/podipu',
+        category: 'Flutter'
     },
     {
         id: 'stockwell',
         name: 'StockWell',
         description: 'StockWell is a mobile application that helps you properly store your foods step-by-step. Powered by Gemini AI, StockWell easily detects your food from images and then will generate all storage steps for each of your foods. StockWell will also remind you about your food\'s shelf life through notifications. So, no more food waste, save your money, and enjoy fresher foods.',
-        url: 'https://youtu.be/BwnXEvk9EVo'
+        url: 'https://youtu.be/BwnXEvk9EVo',
+        category: 'Flutter'
     },
     {
         id: 'simata',
         name: 'SIMATA',
         description: 'SIMATA is a thesis management app for Fakultas MIPA, Universitas Syiah Kuala. This app is adapted from its website, and develop using refactoring method. This mobile app could only be used by lecturers and students in FMIPA USK.',
-        url: 'https://youtu.be/fE7p2KKabT4'
+        url: 'https://youtu.be/fE7p2KKabT4',
+        category: 'Flutter'
     },
     {
         id: 'todo',
         name: 'Simple Todo',
         description: 'Clean and simple todo app for helping people more focus on their todos. User can manage their todos easily, and set priority. This todo app is made using Flutter and local storage.',
-        url: 'https://play.google.com/store/apps/details?id=com.nafladiva.todo'
+        url: 'https://play.google.com/store/apps/details?id=com.nafladiva.todo',
+        category: 'Flutter'
     },
     {
         id: 'ditonton',
         name: 'Ditonton',
         description: 'Ditonton is a mobile app that provide information about latest movies and tv series with TheMoviesDB\'s API. This app is made due to submission for Flutter expert class in Dicoding. With this submission, we, as developer, have to continue the existing project for adding up more features. This project also simulates how a new developer get into a project that already been developed before, like how to adapted to the code, to follow the project flows, etc.',
-        url: 'https://github.com/nafladiva/ditonton-dicoding-MFDE'
+        url: 'https://github.com/nafladiva/ditonton-dicoding-MFDE',
+        category: 'Flutter'
     },
     {
         id: 'sirema',
         name: 'SIREMA (Sistem Reservasi Asrama)',
         description: 'SIREMA (Sistem Reservasi Asrama) is a website for managing hostel reservation in Puslatbang KHAN LAN RI. This web also contains function for reserving facility rooms, such as theater room, classroom, etc. This project was made for KKP (Kuliah Kerja Praktek) in my college.',
-        url: '#'
+        url: '#',
+        category: 'Python'
     },
     {
         id: 'neumize',
         name: 'Neumize',
         description: 'Neumize is a webtool for making picture in a neumorphism style, by just upload your image, edit and it\'s ready to be publish. This website was made by using Bootstrap and Flask.',
-        url: 'http://www.neumize.com'
+        url: 'http://www.neumize.com',
+        category: 'Python'
     },
     {
         id: 'finnote',
         name: 'Finnote',
         description: 'Finnote is a website for managing your money, such as record all your income and expenses and input all your dependent cost that you have to pay, then display it in a visual data so that you could see how your financial condition. This website is made for Re-Cloud Challenge Indonesia 2021 that held by Alibaba Cloud Indonesia, and it was made by using Bootstrap and Django.',
-        url: 'https://github.com/nafladiva/finnote/'
+        url: 'https://github.com/nafladiva/finnote/',
+        category: 'Python'
     },
     {
         id: 'kuoto',
         name: 'Kuoto',
         description: 'Kuoto is a website for generating random quotes from Public API. This website is made for completing the course in Dicoding Indonesia (Belajar Fundamental Front-End Web Development). Kuoto was made by using ES6, Web Component, and Build Tools as taught in the course.',
-        url: '#'
+        url: '#',
+        category: 'React'
     },
     {
         id: 'muvila',
         name: 'Muvila',
         description: 'Muvila is a simple app that provide information about movies. This app also has feature for randomize movie that could be a choice for you to watch, if you are confused about choosing. Muvila is built by using Flutter.',
-        url: 'https://github.com/nafladiva/movielist-flutter'
+        url: 'https://github.com/nafladiva/movielist-flutter',
+        category: 'Flutter'
     },
     {
         id: 'airbud',
         name: 'Airbud',
         description: 'Airbud is a website where we can see the level of quality of air in certain areas/city. This website is built by using Django.',
-        url: 'https://github.com/nafladiva/Airbud'
+        url: 'https://github.com/nafladiva/Airbud',
+        category: 'Python'
     },
     {
         id: 'cocheck',
         name: 'Co-Check',
         description: 'Co-Check is a simple android app which has a feature in the form of checking their condition based on the symptoms that the user has, so that they know what to do next. This Co-Check application also has a feature to inform the latest total cases regarding Covid-19, that obtained from public API. This app is built by using Java.',
-        url: 'https://github.com/nafladiva/co-check'
+        url: 'https://github.com/nafladiva/co-check',
+        category: 'Android'
     },
     {
         id: 'restail',
         name: 'Restail',
         description: 'Restail is a Mobile app that provide information about restaurant, and using Dicoding\'s API from retrieving data. In this project, I learned how to use state management using provider, local storage, custom widget, animation, testing and many more. This app is made using Flutter.',
-        url: 'https://github.com/nafladiva/restaurant-app-flutter'
+        url: 'https://github.com/nafladiva/restaurant-app-flutter',
+        category: 'Flutter'
     }
 ]
 
--- src/ui/sections/ProjectSection.jsx
+++ src/ui/sections/ProjectSection.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectBox from '../components/ProjectBox'
+import projects from '../../data/ProjectData'
 import podipu from '../../images/projects/podipu.png';
 import stockwell from '../../images/projects/stockwell.png';
 import simata from '../../images/projects/simata.png'
@@ -9,28 +10,70 @@ import finnote from '../../images/finnote.png'
 import kuoto from '../../images/kuoto.png'
 import restail from '../../images/restail.png'
 
+const projectImages = {
+    podipu,
+    stockwell,
+    simata,
+    todo,
+    ditonton,
+    finnote,
+    kuoto,
+    restail
+}
+
+const mockUpProjects = {
+    finnote,
+    kuoto,
+    restail
+}
+
 function ProjectSection() {
+    const [activeTab, setActiveTab] = useState('All')
+
+    const categories = ['All', 'Flutter', 'React', 'Python']
+
+    const filteredProjects = activeTab === 'All'
+        ? projects.filter(project => projectImages[project.id])
+        : projects.filter(project => project.category === activeTab && projectImages[project.id])
+
     return (
         <div className="mt-5 col-12 col-md-8 container about-container" id='projectsect'>
             <h4 className='text-center'><b>Projects</b></h4>
 
+            {/* Tab Navigation */}
+            <div className="d-flex justify-content-center mb-4">
+                <div className="btn-group" role="group">
+                    {categories.map(category => (
+                        <button
+                            key={category}
+                            type="button"
+                            className={`btn ${activeTab === category ? 'btn-primary' : 'btn-outline-primary'}`}
+                            onClick={() => setActiveTab(category)}
+                        >
+                            {category}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
             <div className='row'>
-                <ProjectBox name='Podipu' image={podipu} id='podipu' bgColor='ADC4CE' isMockup={true} />
-                <ProjectBox name='StockWell' image={stockwell} id='stockwell' bgColor='7EAA92' isMockup={true} />
-                <ProjectBox name='SIMATA' image={simata} id='simata' bgColor='7EAA92' isMockup={true} />
-                <ProjectBox name='Simple Todo' image={todo} id='todo' bgColor='ADC4CE' isMockup={true} />
-                <ProjectBox name='Ditonton' image={ditonton} id='ditonton' bgColor='FFD89C' isMockup={true} />
-                {/* <ProjectBox name='Neumize' image={neumize} url='http://www.neumize.com/' id='neumize' /> */}
-                <ProjectBox name='Restail' image={restail} url='https://github.com/nafladiva/restaurant-app-flutter' id='restail' />
-                <ProjectBox name='Finnote' image={finnote} url='http://47.254.194.52/' id='finnote' />
-                <ProjectBox name='Kuoto' image={kuoto} id='kuoto' />
-                {/* <ProjectBox name='Muvila' image={muvila} url='https://github.com/nafladiva/movielist-flutter' id='muvila' />
-                <ProjectBox name='Airbud' image={airbud} url='https://github.com/nafladiva/Airbud' id='airbud' />
-                <ProjectBox name='Co-Check' image={cocheck} url='https://github.com/nafladiva/co-check' id='cocheck' /> */}
+                {filteredProjects.map(project => (
+                    <ProjectBox
+                        key={project.id}
+                        name={project.name}
+                        image={projectImages[project.id]}
+                        id={project.id}
+                        bgColor={project.id === 'ditonton' ? 'FFD89C' : (project.category === 'Flutter' ? 'ADC4CE' : '7EAA92')}
+                        isMockup={!(project.id in mockUpProjects)}
+                        url={project.url}
+                    />
+                ))}
             </div>
 
             <div className="text-center">
-                <p className="mt-5 text-center">more project still on going...</p>
+                {activeTab === 'All' && (
+                    <p className="mt-5 text-center">more project still on going...</p>
+                )}
                 {/* <Button text='See more' tag='sec' to='' /> */}
             </div>
         </div>
